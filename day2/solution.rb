feb3d5d25c460479fd2f77778bd45415df2bb009
PlanetEfficacy <jspevack@gmail.com>
2022-12-01 23:19:24 -0700

    Part 2 Day 2

--- day2/solution.rb
+++ day2/solution.rb
@@ -1,84 +1,189 @@
 require 'pry'
-class Solution
-  def initialize(path)
-    @path = path
-  end
 
-  def call 
-    score = 0
-    File.readlines(@path).each do |line|
-      opponent_throw, your_throw = line.split(' ').map { |letter_code| Throw.new(letter_code) }
-      match = Match.new(opponent_throw, your_throw)
-      round_score = match.points + your_throw.points
-      score += round_score
+module Part1
+  class Solution
+    def initialize(path)
+      @path = path
     end
 
-    return score
+    def call 
+      score = 0
+      File.readlines(@path).each do |line|
+        opponent_throw, your_throw = line.split(' ').map { |letter_code| Throw.new(letter_code) }
+        match = Match.new(opponent_throw, your_throw)
+        round_score = match.points + your_throw.points
+        score += round_score
+      end
+
+      return score
+    end
+
   end
 
+  class Throw
+    DICTIONARY = {
+      "A" => :rock,
+      "B" => :paper,
+      "C" => :scissors,
+      "X" => :rock,
+      "Y" => :paper,
+      "Z" => :scissors,
+    }
+
+    SHAPE_POINTS = {
+      rock: 1,
+      paper: 2,
+      scissors: 3,
+    }
+
+    def initialize(letter_code)
+      @letter_code = letter_code
+    end
+
+    def shape
+      DICTIONARY[@letter_code]
+    end
+
+    def points
+      SHAPE_POINTS[shape]    
+    end
+  end
+
+  class Match
+    def initialize(opponent_throw, your_throw)
+      @opponent_throw = opponent_throw.shape
+      @your_throw = your_throw.shape
+    end
+
+    def points
+      return 6 if won?
+      return 3 if tie?
+      0
+    end
+
+    private
+
+    def won?
+      winner == :you
+    end
+
+    def tie?
+      winner == false
+    end
+
+    def winner
+      return false if @opponent_throw == @your_throw
+
+      case @opponent_throw
+      when :rock
+        @your_throw == :paper ? :you : :opponent
+      when :paper 
+        @your_throw == :scissors ? :you : :opponent
+      when :scissors 
+        @your_throw == :rock ? :you : :opponent
+      end
+    end
+  end
 end
 
-class Throw
-  DICTIONARY = {
-    "A" => :rock,
-    "B" => :paper,
-    "C" => :scissors,
-    "X" => :rock,
-    "Y" => :paper,
-    "Z" => :scissors,
-  }
+module Part2
+  class Solution < Part1::Solution
+    def call
+      score = 0
+      File.readlines(@path).each do |line|
+        opponent_code, your_code = line.split(' ')
+        match = Match.new(Throw.new(opponent_code), Intention.new(your_code))
+        round_score = match.shape_points + match.outcome_points 
+        puts round_score
+        score += round_score
+      end
 
-  SHAPE_POINTS = {
-    rock: 1,
-    paper: 2,
-    scissors: 3,
-  }
-
-  def initialize(letter_code)
-    @letter_code = letter_code
+      return score
+    end
   end
 
-  def shape
-    DICTIONARY[@letter_code]
+  class Intention
+    DICTIONARY = {
+      "X" => :lose,
+      "Y" => :draw,
+      "Z" => :win,
+    }
+
+    def initialize(letter_code)
+      @intent = DICTIONARY[letter_code]
+    end
+
+    def win?
+      @intent == :win
+    end
+
+    def draw?
+      @intent == :draw
+    end
   end
 
-  def points
-    SHAPE_POINTS[shape]    
-  end
-end
+  class Throw
+    DICTIONARY = {
+      "A" => :rock,
+      "B" => :paper,
+      "C" => :scissors,
+    }
 
-class Match
-  def initialize(opponent_throw, your_throw)
-    @opponent_throw = opponent_throw.shape
-    @your_throw = your_throw.shape
-  end
+    def initialize(letter_code)
+      @letter_code = letter_code
+    end
 
-  def points
-    return 6 if won?
-    return 3 if tie?
-    0
-  end
+    def shape
+      DICTIONARY[@letter_code]
+    end
 
-  private
-
-  def won?
-    winner == :you
+    def points
+      SHAPE_POINTS[shape]    
+    end
   end
 
-  def tie?
-    winner == false
-  end
+  class Match
+    SHAPE_POINTS = {
+      rock: 1,
+      paper: 2,
+      scissors: 3,
+    }
 
-  def winner
-    return false if @opponent_throw == @your_throw
+    WINNER = {
+      rock: :paper,
+      paper: :scissors,
+      scissors: :rock,
+    }
 
-    case @opponent_throw
-    when :rock
-      @your_throw == :paper ? :you : :opponent
-    when :paper 
-      @your_throw == :scissors ? :you : :opponent
-    when :scissors 
-      @your_throw == :rock ? :you : :opponent
+    LOSER = {
+      rock: :scissors,
+      paper: :rock,
+      scissors: :paper,
+    }
+
+    def initialize(opponent_throw, your_intention)
+      @opponent_throw = opponent_throw
+      @your_intention = your_intention
+    end
+
+    def outcome_points
+      if @your_intention.win?
+        6
+      elsif @your_intention.draw?
+        3
+      else
+        0
+      end
+    end
+
+    def shape_points
+      if @your_intention.win?
+        SHAPE_POINTS[WINNER[@opponent_throw.shape]]
+      elsif @your_intention.draw?
+        SHAPE_POINTS[@opponent_throw.shape]
+      else
+        SHAPE_POINTS[LOSER[@opponent_throw.shape]]
+      end
     end
   end
 end
