bd701234c9e64b02ff02f97f467bd206dc0a35fe
Jesse Spevack <jspevack@gmail.com>
2022-12-10 09:07:00 -0800

    Update solution.rb

--- day8/solution.rb
+++ day8/solution.rb
@@ -55,7 +55,64 @@ module Part2Solution
     end
 
     def call
-      true
+      grid = []
+      File.readlines(path).each do |line|
+        trees = line.chomp.chars
+        grid << trees
+      end
+
+      height = grid.count
+      width = grid.first.count
+
+      max_score = 0
+      all = []
+
+      grid.each_with_index do |row, i|
+        row.each_with_index do |t, j|
+          tree = t.to_i
+          score = { tree: t.to_i, up: 0, left: 0, right: 0, down: 0, i: i, j: j }
+          column = grid.map { |r| r[j] }
+
+          # up
+          (0...i).to_a.reverse.each do |k|
+            neighbor = column[k].to_i
+            score[:up] += 1
+
+            break if neighbor >= tree.to_i
+          end
+
+          # left
+          (0...j).to_a.reverse.each do |k|
+            neighbor = row[k].to_i
+            score[:left] += 1
+
+            break if neighbor >= tree.to_i
+          end
+
+          # right
+          ((j + 1)...width).to_a.each do |k|
+            neighbor = row[k].to_i
+            score[:right] += 1
+
+            break if neighbor >= tree.to_i
+          end
+
+          # down
+          ((i + 1)...height).to_a.each do |k|
+            neighbor = column[k].to_i
+            score[:down] += 1
+
+            break if neighbor >= tree.to_i
+          end
+
+          total_score = score[:up] * score[:down] * score[:left] * score[:right]
+          score[:total_score] = total_score
+          all << score
+          max_score = total_score if total_score > max_score
+        end
+      end
+
+      max_score
     end
   end
 end
